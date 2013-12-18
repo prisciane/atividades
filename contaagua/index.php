36aa3ed495e6b138cc6fbeb32d68300ad3e1faad
prisciane <priscianne_lima@hotmail.com>
2013-12-18 16:19:50 -0200

    Create index.php

---
+++ contaagua/index.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+      <title>calculadora de agua</title>
+</head>
+
+
+<body>
+<form action="programa.php" method="post">
+      <fieldset>
+      <h3>calculadora de agua</h3>
+      <h4>por favor,digite os valores das contas </h4>
+
+      <label>primeiro mes</label> <input type="text" name="m1"
+ /> <br/>
+      <label>segundo mes</label> <input type="text" name="m2"
+/> <br/>
+      <label>terceiro mes <label> &nbsp;<input type= "text" name="m3" /> <br/> <br/>
+       </fieldset>
+             <input type="submit" value="enviar" />
+       </form>
+</body>
+</html>
+                 
