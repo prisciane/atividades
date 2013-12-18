91fc436645332353363193f27af66aeb99c26ffd
prisciane <priscianne_lima@hotmail.com>
2013-12-18 16:27:09 -0200

    Create index.php

---
+++ texto/index.php
@@ -0,0 +1,17 @@
+<?php
+$x = 9;
+$y = 12;
+
+$dia = 11;
+$mes = 12;
+$ano= 2013;
+$texto = "A data de hoje é ";
+
+echo $texto . $dia . "/" . $mes."/" . $ano;
+echo "<br/>";
+echo "O dia de hoje é $dia/$mes/$ano";
+echo "<br/>";
+echo "$x + $y = ' . ($x + $y);
+
+
+?>
